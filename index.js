91e139b03d9e5125c04f6d35df3583cc82783cb7
Andrei2381283 <87148148+Andrei2381283@users.noreply.github.com>
2023-12-20 14:47:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const findMissingNum = (arr) => {
   for(const num of arr) {
     if(obj[num]) throw 'Array has duplicates';
     obj[num] = true; // Put numbers to object for easy and fast access
-    if(typeof num !== 'number') throw 'Array contains non number value'; // Non number values can break function, throw null
+    if(typeof num !== 'number') throw 'Array contains non number value'; // Throw error because non number values can break function
     if(num < minNum) minNum = num; // Find minimum number in array for ignore
   }
   for(const num of arr) {
     if(num === minNum) continue; // Ignore minimum number
-    if(!obj[num - 2] && num > minNum + 2) throw 'Multiple answers'; // Throw null because array contains multiple missing values
+    if(!obj[num - 2] && num > minNum + 2) throw 'Multiple answers'; // Throw error because array contains multiple missing values
     if(!obj[num - 1]) return num - 1; // Find expected number
   }
 
